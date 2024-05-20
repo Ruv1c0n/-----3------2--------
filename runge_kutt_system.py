b795f56f1118d4fe570ea7c453c4f31f71064928
Daniil Matveev <60100551+Ruv1c0n@users.noreply.github.com>
2024-05-20 19:31:42 +0300

    some refactor

--- runge_kutt_system.py
+++ runge_kutt_system.py
@@ -11,18 +11,18 @@ def f_x(x, y, t=0):
 def f_y(x, y, t=0):
     return (x-4)*(y-x)
 
-def Runge_kutt4(x_0, y_0, a, b, h):
+def Runge_kutt4(x_0, y_0, t, a, b, h):
     points_x, points_y = [], []
     step = a
     while step < b:
-        k1 = h * f_x(x_0, y_0)
-        q1 = h * f_y(x_0, y_0)
-        k2 = h * f_x(x_0 + k1 / 3, y_0 + q1 / 3)
-        q2 = h * f_y(x_0 + k1 / 3, y_0 + q1 / 2)
-        k3 = h * f_x(x_0 - k1 / 2 + k2, y_0 - q1 / 2 + q2)
-        q3 = h * f_y(x_0 - k1 / 2 + k2, y_0 - q1 / 2 + q2)
-        k4 = h * f_x(x_0 + k1 - k2 + k3, y_0 + q1 - q2 + q3)
-        q4 = h * f_y(x_0 + k1 - k2 + k3, y_0 + q1 - q2 + q3)
+        k1 = h * f_x(x_0, y_0, t)
+        q1 = h * f_y(x_0, y_0, t)
+        k2 = h * f_x(x_0 + k1 / 3, y_0 + q1 / 3, t)
+        q2 = h * f_y(x_0 + k1 / 3, y_0 + q1 / 2, t)
+        k3 = h * f_x(x_0 - k1 / 2 + k2, y_0 - q1 / 2 + q2, t)
+        q3 = h * f_y(x_0 - k1 / 2 + k2, y_0 - q1 / 2 + q2, t)
+        k4 = h * f_x(x_0 + k1 - k2 + k3, y_0 + q1 - q2 + q3, t)
+        q4 = h * f_y(x_0 + k1 - k2 + k3, y_0 + q1 - q2 + q3, t)
         x_0 += (k1 + 3 * k2 + 3 * k3 + k4) / 8
         y_0 += (q1 + 3 * q2 + 3 * q3 + q4) / 8
         points_x.append(x_0)
@@ -35,6 +35,7 @@ def solveWequilibrium():
     h = 0.05
     a, b = 0, 200
     shift = 1
+    t = 0
     equilibrium_points = np.array([[-2, -2], [2, 2], [4, 1]])
     fig = plt.figure(1, (10, 10))
     plt.xlim(*xy_range)
@@ -45,7 +46,7 @@ def solveWequilibrium():
     )
     plt.streamplot(
         X, Y,
-        f_x(X, Y), f_y(X, Y),
+        f_x(X, Y, t), f_y(X, Y, t),
         color='cyan',
         density=1,
         arrowstyle='->',
@@ -53,51 +54,62 @@ def solveWequilibrium():
     )
 
     for i in range(len(equilibrium_points)):
-        plt.pause(1)
         Runge_kutt4(
             equilibrium_points[i][0], 
             equilibrium_points[i][1], 
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0],
             equilibrium_points[i][1] - shift, 
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0],
             equilibrium_points[i][1] + shift, 
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0] - shift, 
-            equilibrium_points[i][1], a, b, h)
+            equilibrium_points[i][1], 
+            t,
+            a, b, h
+        )
         Runge_kutt4(
             equilibrium_points[i][0] + shift, 
             equilibrium_points[i][1], 
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0] - shift, 
             equilibrium_points[i][1] - shift, 
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0] - shift, 
             equilibrium_points[i][1] + shift, 
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0] + shift, 
             equilibrium_points[i][1] - shift, 
+            t,
             a, b, h
             )
         Runge_kutt4(
             equilibrium_points[i][0] + shift, 
             equilibrium_points[i][1] + shift, 
+            t,
             a, b, h
         )
         shift -= 0.4
+        plt.pause(1)
 
     plt.plot(
         *np.hsplit(equilibrium_points, 2),
@@ -112,8 +124,10 @@ def solveWOUTequilibrium(dotx, doty):
     h = 0.05
     a, b = 0, 200
     shift = 1
+    t = 0
     points = np.array(sympy.solve([dotx, doty], ['x', 'y'], check=False))
-    equilibrium_points = np.array([[*p] for p in points])
+    equilibrium_points = np.array([[int(p[0]), int(p[1])] for p in points])
+    print(type(equilibrium_points[0][0]))
     fig = plt.figure(1, (10, 10))
     plt.xlim(*xy_range)
     plt.ylim(*xy_range)
@@ -134,47 +148,59 @@ def solveWOUTequilibrium(dotx, doty):
         Runge_kutt4(
             equilibrium_points[i][0],
             equilibrium_points[i][1],
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0],
             equilibrium_points[i][1] - shift,
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0],
             equilibrium_points[i][1] + shift,
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0] - shift,
-            equilibrium_points[i][1], a, b, h)
+            equilibrium_points[i][1],
+            t, 
+            a, b, h
+        )
         Runge_kutt4(
             equilibrium_points[i][0] + shift,
             equilibrium_points[i][1],
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0] - shift,
             equilibrium_points[i][1] - shift,
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0] - shift,
             equilibrium_points[i][1] + shift,
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0] + shift,
             equilibrium_points[i][1] - shift,
+            t,
             a, b, h
         )
         Runge_kutt4(
             equilibrium_points[i][0] + shift,
             equilibrium_points[i][1] + shift,
+            t,
             a, b, h
         )
         shift -= 0.4
+        plt.pause(1)
 
     plt.plot(
         *np.hsplit(equilibrium_points, 2),
@@ -186,4 +212,4 @@ def solveWOUTequilibrium(dotx, doty):
 
 
 solveWequilibrium()
-# solveWOUTequilibrium('x*y - 4', '(x-4)*(y-x)')
+solveWOUTequilibrium('x*y - 4', '(x-4)*(y-x)')
